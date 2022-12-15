500209833633c130804a6ce112947f3372aa6551
nityaveera <94888714+nityaveera@users.noreply.github.com>
2022-12-15 16:39:10 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y openjdk-11-jdk
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins 
+sudo systemctl start jenkins
+sudo systemctl status jenkins
